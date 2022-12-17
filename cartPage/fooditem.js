39215adf18e1e0be3efe73e830fbc1c0ee23fd01
Jun <kc50049@gmail.com>
2022-12-17 22:08:23 +0800

    1

---
+++ cartPage/fooditem.js
@@ -0,0 +1,404 @@
+const foodItem= [
+// {
+//     id: 1,
+//     name: 'Ambur ',
+//     category : '中式料理',
+//     rating : 4.3,
+//     price: 13,
+//     img: './images/biryani/Ambur-Chicken-Biryani.jpg',
+//     quantity: 1
+// },
+{
+    id: 2,
+    name: '炒飯',
+    category : '中式料理',
+    rating : 4.3,
+    price: 90,
+    img: './images-cart/chinese/chinese1.jpg',
+    quantity: 1
+},
+{
+    id: 3,
+    name: '炒麵',
+    category : '中式料理',
+    rating : 4.3,
+    price: 90,
+    img: './images-cart/chinese/chinese2.jpg',
+    quantity: 1
+},
+// {
+//     id: 4,
+//     name: 'Goan Fish Biryani',
+//     category : '中式料理',
+//     rating : 4.3,
+//     price: 15,
+//     img: './images/biryani/goan-fish-biryani.jpg',
+//     quantity: 1
+// },
+// {
+//     id: 5,
+//     name: 'Mutton Biryani',
+//     category : '中式料理',
+//     rating : 4.3,
+//     price: 10,
+//     img: './images/biryani/hyd-Mutton-Biryani.jpg',
+//     quantity: 1
+// },
+// // {
+// //     id: 6,
+// //     name: 'Kamrupi Biryani',
+// //     category : '中式料理',
+// //     rating : 4.3,
+// //     price: 12,
+// //     img: './images/biryani/kamrupi-biryani.jpg',
+// //     quantity: 1
+// // },
+// {
+//     id: 7,
+//     name: 'Kashmiri Biryani',
+//     category : '中式料理',
+//     rating : 4.3,
+//     price: 13,
+//     img: './images/biryani/kashmiri.pulao.jpg',
+//     quantity: 1
+// },
+// {
+//     id: 8,
+//     name: 'Memoni Biryani',
+//     category : '中式料理',
+//     rating : 4.3,
+//     price: 20,
+//     img: './images/biryani/memonibiryani.png',
+//     quantity: 1
+// },
+// {
+//     id: 9,
+//     name: 'Mughlai Biryani',
+//     category : '中式料理',
+//     rating : 4.3,
+//     price: 15,
+//     img: './images/biryani/mughlai-biryani.jpg',
+//     quantity: 1
+// },
+{
+    id: 10,
+    name: '漢堡',
+    category : '西式料理',
+    rating : 4.3,
+    price: 90,
+    img: './images-cart/west/west1.jpg',
+    quantity: 1
+},
+{
+    id: 11,
+    name: '披薩',
+    category : '西式料理',
+    rating : 4.3,
+    price: 90,
+    img: './images-cart/west/west2.jpg',
+    quantity: 1
+},
+// {
+//     id: 12,
+//     name: 'Chicken Do Pyaza',
+//     category : 'chicken',
+//     rating : 4.3,
+//     price: 14,
+//     img: './images/chicken/Chicken-do-Pyaza.jpg',
+//     quantity: 1
+// },
+// {
+//     id: 13,
+//     name: 'Chicken Masala',
+//     category : 'chicken',
+//     rating : 4.3,
+//     price: 12,
+//     img: './images/chicken/Chicken-Masala.jpeg',
+//     quantity: 1
+// },
+// {
+//     id: 14,
+//     name: 'Handi Chicken',
+//     category : 'chicken',
+//     rating : 4.3,
+//     price: 17,
+//     img: './images/chicken/Handi-chicken.jpg',
+//     quantity: 1
+// },
+// {
+//     id: 15,
+//     name: 'Murgh Musallam',
+//     category : 'chicken',
+//     rating : 4.3,
+//     price: 20,
+//     img: './images/chicken/Murgh-Musallam.jpg',
+//     quantity: 1
+// },
+{
+    id: 16,
+    name: '火鍋',
+    category : '韓式料理',
+    rating : 4.3,
+    price: 90,
+    img: './images-cart/korea/korea1.jpg',
+    quantity: 1
+
+},
+{
+    id: 17,
+    name: '韓式炸雞',
+    category : '韓式料理',
+    rating : 4.3,
+    price: 90,
+    img: './images-cart/korea/korea2.png',
+    quantity: 1
+},
+// {
+//     id: 18,
+//     name: 'Paneer Butter Masala',
+//     category : 'paneer',
+//     rating : 4.3,
+//     price: 15,
+//     img: './images/paneer/paneer-butter-masala.jpg',
+//     quantity: 1
+
+// },
+// {
+//     id: 19,
+//     name: 'Paneer Do Pyaza',
+//     category : 'paneer',
+//     rating : 4.3,
+//     price: 12,
+//     img: './images/paneer/Paneer-Do-Pyaza.jpg',
+//     quantity: 1
+
+// },
+// {
+//     id: 20,
+//     name: 'Hyderabadi Paneer',
+//     category : 'paneer',
+//     rating : 4.3,
+//     price: 8,
+//     img: './images/paneer/Paneer-Hyderabadi.jpg',
+//     quantity: 1
+// },
+// {
+//     id: 21,
+//     name: 'Paneer Lababdar',
+//     category : 'paneer',
+//     rating : 4.3,
+//     price: 7,
+//     img: './images/paneer/paneer-lababdar.jpg',
+//     quantity: 1
+// },
+// {
+//     id: 22,
+//     name: 'Shahi Paneer',
+//     age: '32',
+//     category : 'paneer',
+//     rating : 4.3,
+//     price: 5,
+//     img: './images/paneer/shahi-paneer.jpg',
+//     quantity: 1
+// },
+{
+    id: 23,
+    name: '沙拉',
+    category : '素食',
+    rating : 4.3,
+    price: 90,
+    img: './images-cart/vegetable/veg1.jpg',
+    quantity: 1
+},
+{
+    id: 24,
+    name: '燉菜',
+    category : '素食',
+    rating : 4.3,
+    price: 90,
+    img: './images-cart/vegetable/veg2.jpg',
+    quantity: 1
+},
+// {
+//     id: 25,
+//     name: 'Veg Biryani',
+//     category : 'vegetable',
+//     rating : 4.3,
+//     price: 5,
+//     img: './images/vegetable/vegetable-biryani.jpg',
+//     quantity: 1
+// },
+// {
+//     id: 26,
+//     name: 'Veg Curry',
+//     category : 'vegetable',
+//     rating : 4.3,
+//     price: 7,
+//     img: './images/vegetable/vegetable-curry.jpeg',
+//     quantity: 1
+// },
+// {
+//     id: 27,
+//     name: 'Veg Kolhapur',
+//     category : 'vegetable',
+//     rating : 4.3,
+//     price: 10,
+//     img: './images/vegetable/vegetable-kolhapuri.jpg',
+//     quantity: 1
+// },
+// {
+//     id: 28,
+//     name: 'Veg Masala',
+//     category : 'vegetable',
+//     rating : 4.3,
+//     price: 4,
+//     img: './images/vegetable/vegetable-masala.jpg',
+//     quantity: 1
+// },
+// {
+//     id: 29,
+//     name: 'Veg Pakora',
+//     category : 'vegetable',
+//     rating : 4.3,
+//     price: 4,
+//     img: './images/vegetable/vegetable-pakora.jpg',
+//     quantity: 1
+// },
+{
+    id: 30,
+    name: '河粉',
+    category : '南洋風味',
+    rating : 4.3,
+    price: 90,
+    img: './images-cart/south indian/south1.jpg',
+    quantity: 1
+},
+{
+    id: 31,
+    name: '蝦餅',
+    category : '南洋風味',
+    rating : 4.3,
+    price: 90,
+    img: './images-cart/south indian/south2.jpg',
+    quantity: 1
+},
+// {
+//     id: 32,
+//     name: 'Chili Chicken',
+//     category : 'chinese',
+//     rating : 4.3,
+//     price: 5,
+//     img: './images/chinese/Chili-Chicken.jpg',
+//     quantity: 1
+// },
+// {
+//     id: 33,
+//     name: 'Chowmein',
+//     category : 'chinese',
+//     rating : 4.3,
+//     price: 16,
+//     img: './images/chinese/chowmin.jpg',
+//     quantity: 1
+// },
+// {
+//     id: 34,
+//     name: 'Spring Roll',
+//     category : 'chinese',
+//     rating : 4.3,
+//     price: 14,
+//     img: './images/chinese/spring-rolls.jpg',
+//     quantity: 1
+// },
+// {
+//     id: 35,
+//     name: 'Szechuan Chicken',
+//     category : 'chinese',
+//     rating : 4.3,
+//     price: 10,
+//     img: './images/chinese/szechuan-chicken.jpg',
+//     quantity: 1
+// },
+// {
+//     id: 36,
+//     name: 'Fried Rice',
+//     category : 'chinese',
+//     rating : 4.3,
+//     price: 8,
+//     img: './images/chinese/veg-fried-rice.jpg',
+//     quantity: 1
+// },
+{
+    id: 37,
+    name: '咖哩',
+    category : '印度料理',
+    rating : 4.3,
+    price: 90,
+    img: './images-cart/india/india1.jpg',
+    quantity: 1
+},
+{
+    id: 38,
+    name: '香料烤雞',
+    category : '印度料理',
+    rating : 4.3,
+    price: 20,
+    img: './images-cart/india/india2.jpg',
+    quantity: 1
+},
+// {
+//     id: 39,
+//     name: 'Masala Dosa',
+//     category : 'south indian',
+//     rating : 4.3,
+//     price: 12,
+//     img: './images/south indian/masala-dosa.jpg',
+//     quantity: 1
+// },
+// {
+//     id: 40,
+//     name: 'Mysore Bonda',
+//     category : 'south indian',
+//     rating : 4.3,
+//     price: 10,
+//     img: './images/south indian/mysore-bonda.jpg',
+//     quantity: 1
+// },
+// {
+//     id: 41,
+//     name: 'Onion Uttapam',
+//     category : 'south indian',
+//     rating : 4.3,
+//     price: 15,
+//     img: './images/south indian/onion-uttapam.jpg',
+//     quantity: 1
+// },
+// {
+//     id: 42,
+//     name: 'Plain Dosa',
+//     category : 'south indian',
+//     rating : 4.3,
+//     price: 40,
+//     img: './images/south indian/plain-dosa.jpeg',
+//     quantity: 1
+// },
+// {
+//     id: 43,
+//     name: 'Rava Uttapam',
+//     category : 'south indian',
+//     rating : 4.3,
+//     price: 25,
+//     img: './images/south indian/Rava-Uttapam.jpg',
+//     quantity: 1
+// },
+// {
+//     id: 44,
+//     name: 'Sambhar Vada',
+//     category : 'south indian',
+//     rating : 4.3,
+//     price: 34,
+//     img: './images/south indian/sambhar-vada.jpg',
+//     quantity: 1
+// },
+]
+
+export {foodItem};
